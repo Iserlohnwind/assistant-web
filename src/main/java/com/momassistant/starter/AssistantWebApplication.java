95917ca867ad765152351b9aa4afa9293a995219
zhufeng <zhufeng@meituan.com>
2018-08-14 19:31:54 +0800

    upd

--- src/main/java/com/momassistant/starter/AssistantWebApplication.java
+++ src/main/java/com/momassistant/starter/AssistantWebApplication.java
@@ -1,11 +1,12 @@
 package com.momassistant.starter;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.momassistant")
+@MapperScan("com.momassistant.mapper")
 public class AssistantWebApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/momassistant/starter/AssistantWebApplicationTests.java
+++ src/test/java/com/momassistant/starter/AssistantWebApplicationTests.java
@@ -4,6 +4,7 @@ import com.momassistant.mapper.UserSessionMapper;
 import com.momassistant.mapper.model.UserSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,7 +12,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ComponentScan(basePackages = "com.momassistant")
+//@ComponentScan(basePackages = "com.momassistant")
+@MapperScan("com.momassistant.mapper")
 public class AssistantWebApplicationTests {
 
 	@Autowired
