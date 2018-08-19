63d691113bb772d4e0de32be21985fb129d46372
zhufeng <zhufeng@meituan.com>
2018-08-19 14:58:40 +0800

    upd

--- src/main/java/com/momassistant/controller/ControllerExceptionHandleAdvice.java
+++ src/main/java/com/momassistant/controller/ControllerExceptionHandleAdvice.java
@@ -3,6 +3,8 @@ package com.momassistant.controller;
 import com.momassistant.ReturnCode;
 import com.momassistant.entity.Response;
 import com.momassistant.exception.TokenValidateException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,10 +16,11 @@ import javax.servlet.http.HttpServletResponse;
  */
 @RestControllerAdvice
 public class ControllerExceptionHandleAdvice {
+    protected final Log logger = LogFactory.getLog(this.getClass());
 
     @ExceptionHandler
     public Response handler(HttpServletRequest req, HttpServletResponse res, Exception e) {
-        e.printStackTrace();
+        logger.error("controller error", e);
         if (e instanceof TokenValidateException) {
             return Response.error(ReturnCode.TOKEN_EXPIRED);
         }
